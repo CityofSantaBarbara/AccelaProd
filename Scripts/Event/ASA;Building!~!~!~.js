1f50e1494aadbbd2673b81029f3e0ce377881e9d
SBAccela <ITadmin@SantaBarbaraCA.gov>
2023-12-11 15:47:28 -0800

    Update ASA;Building!~!~!~.js
    Add BLD Application Submit auto email

--- Scripts/Event/ASA;Building!~!~!~.js
+++ Scripts/Event/ASA;Building!~!~!~.js
@@ -23,3 +23,32 @@ if (!publicUser) {
 		}
 	}
 	*/
+
+if (publicUser) {
+    logDebug("Application Received Notification - Begin");
+
+    var capContactResult = aa.people.getCapContactByCapID(capId);
+    if (capContactResult.getSuccess()) {
+        var toEmail = '';
+        var Contacts = capContactResult.getOutput();
+        for (var contaxtIdx in Contacts) {
+            email = Contacts[contaxtIdx].getEmail();
+            if (email != ''){ 
+                break;
+            }
+        }
+        toEmail = email;
+        var fromEmail = lookup("SCRIPT_EMAIL_FROM", "AGENCY_FROM");
+        var ccEmail = ""; //blank for now
+        var emailTemplate = "BLD APPLICATION SUBMIT";
+        var capId4Email = aa.cap.createCapIDScriptModel(capId.getID1(), capId.getID2(), capId.getID3());
+        conEmailStr = toEmail.join(";");
+        ccEmailStr = ccEmail;
+        sendNotification(fromEmail, toEmail, ccEmail, emailTemplate, null, null);
+
+        logDebug("BLD APPLICATION SUBMIT: Sent Email template " + emailTemplate + " To Contacts " + conEmailStr);
+    } else {
+        logDebug("BLD APPLICATION SUBMIT: No contacts found");
+    }
+    logDebug("Application Received Notification - End");
+}

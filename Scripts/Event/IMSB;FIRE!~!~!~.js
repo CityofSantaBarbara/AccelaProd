ef43b4c9a6cdbff8819af614fa973876d9ee0bf9
SBAccela <ITadmin@SantaBarbaraCA.gov>
2024-04-05 14:06:48 -0700

    Create IMSB;FIRE!~!~!~.js

---
+++ Scripts/Event/IMSB;FIRE!~!~!~.js
@@ -0,0 +1,14 @@
+//InspectionMultipleScheduleBefore is a BACK OFFICE (AA) script
+
+/*
+var capStatus = cap.getCapStatus();
+
+if(capStatus != "Issued" && capStatus != "Permit Issued"){
+	//cancel scheduling
+	//logDebug("You Can't Schedule This!!!");	
+	showMessage = true;
+	showDebug = false;
+	comment("Inspections may not be scheduled until a permit has been issued!");
+	cancel = true;
+}
+*/
